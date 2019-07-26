fb62259277b2ab65b0e76268f3d0ab92cc69a116
bwmelon <1454490647@qq.com>
2019-07-26 23:09:36 +0800

    :fire:删除旧版样式多余代码

--- js/index.js
+++ js/index.js
@@ -42,7 +42,7 @@ $(function () {
 		layer.open({
 			type: 1,
 			area: ['310px', '450px'], //宽高
-			content: '<div style="text-align: center;"><br><img src="https://imgs.bwmelon.com/20190516143115.png" style="width:80%;margin-bottom:15px;"><br>"扫码二维码查看支付效果或者打赏"<br>"本站生成的收款码无红包广告"<br>"又拍云CDN加速，收款更快捷"</div>',
+			content: '<div style="text-align: center;"><br><img src="https://imgs.bwmelon.com/20190722123548.png" style="width:80%;margin-bottom:15px;"><br>"扫码二维码查看支付效果或者打赏"<br>"本站生成的收款码无红包广告"<br>"又拍云CDN加速，收款更快捷"</div>',
 			title: '支付宝扫码领红包',
 			btn: ['关闭'],
 			btnAlign: 'c',
@@ -52,58 +52,6 @@ $(function () {
 			}
 		});
 
-		// 调用颜色选择器
-		function colorpicker() {
-			var topColor, bottomColor, recTextColor, bottomTextColor;
-			// 主体颜色
-			colorpicker.render({
-				elem: '#topColorBtn',
-				color: '#5a91eb',
-				predefine: true,
-				colors: ['#5a91eb', '#F00', '#0F0', '#00F', 'rgb(255, 69, 0)'],
-				size: 'lg',
-				change: function (color) {
-					topColor = color;
-					makeBg(topColor, bottomColor, recTextColor, bottomTextColor);
-				}
-			});
-			// 底部颜色
-			colorpicker.render({
-				elem: '#bottomColorBtn',
-				color: '#ffffff',
-				predefine: true,
-				colors: ['#ffffff', '#F00', '#0F0', '#00F', 'rgb(255, 69, 0)'],
-				size: 'lg',
-				change: function (color) {
-					bottomColor = color;
-					makeBg(topColor, bottomColor, recTextColor, bottomTextColor);
-				}
-			});
-			// 收款人文字颜色
-			colorpicker.render({
-				elem: '#recTextColorBtn',
-				color: '#ffffff',
-				predefine: true,
-				colors: ['#ffffff', '#F00', '#0F0', '#00F', 'rgb(255, 69, 0)'],
-				size: 'lg',
-				change: function (color) {
-					recTextColor = color;
-					makeBg(topColor, bottomColor, recTextColor, bottomTextColor);
-				}
-			});
-			// 底部文字颜色
-			colorpicker.render({
-				elem: '#bottomTextColorBtn',
-				color: '#5a91eb',
-				predefine: true,
-				colors: ['#5a91eb', '#F00', '#0F0', '#00F', 'rgb(255, 69, 0)'],
-				size: 'lg',
-				change: function (color) {
-					bottomTextColor = color;
-					makeBg(topColor, bottomColor, recTextColor, bottomTextColor);
-				}
-			});
-		}
 
 		//获取qq收款码链接
 		function getUrl_qq(e, param) {
@@ -192,7 +140,6 @@ $(function () {
 
 		// 上传二维码并解析
 		$("#qqBtn").on('change', function (e) {
-			// getUrl_qq(this, 'file-url');
 			hasImage = false;
 			var imageData = null;
 			var file = e.target.files[0];
@@ -248,112 +195,13 @@ $(function () {
 				}
 			}, 200);
 
-		})
+		});
 		$("#wechatBtn").on('change', function () {
 			getUrl_wechat(this, 'file-url');
 		})
 		$("#aliBtn").on('change', function () {
 			getUrl_ali(this, 'file-url');
 		})
-
-		// 生成收款码（经典样式 可自定义颜色）(当前版本暂时弃用)
-		// function makeBg(topColor = "#5a91eb", bottomColor = "#ffffff", recTextColor = "#ffffff", bottomTextColor = "#5a91eb", recName, url) {
-		// 	// 设置默认属性
-		// 	recName = recName || document.getElementById("recName").value;
-		// 	//jQuery生成二维码         
-		// 	$('#code').qrcode({
-		// 		render: "canvas",
-		// 		width: 550,
-		// 		height: 550,
-		// 		text: url,
-		// 		foreground: "black",
-		// 		background: "white"
-		// 	});
-		// 	// 开始绘制展示页面
-		// 	var canvas = document.getElementById("canvas");
-		// 	var ctx = canvas.getContext('2d');
-		// 	// 上矩形
-		// 	ctx.fillStyle = topColor;
-		// 	ctx.fillRect(0, 0, 900, 1090);
-		// 	// 下矩形
-		// 	ctx.fillStyle = bottomColor;
-		// 	ctx.fillRect(0, 1090, 900, 130);
-		// 	// 二维码矩形
-		// 	ctx.fillStyle = "#ffffff";
-		// 	ctx.fillRect(140, 200, 600, 600);
-		// 	// 微信
-		// 	ctx.textAlign = "center";
-		// 	ctx.font = "40px '黑体'";
-		// 	ctx.fillStyle = "#ffffff";
-		// 	ctx.fillText("微信", 226, 1052);
-		// 	// 支付宝
-		// 	ctx.font = "40px '黑体'";
-		// 	ctx.fillStyle = "#ffffff";
-		// 	ctx.fillText("支付宝", 450, 1052);
-		// 	// QQ钱包
-		// 	ctx.font = "40px '黑体'";
-		// 	ctx.fillStyle = "#ffffff";
-		// 	ctx.fillText("QQ钱包", 672, 1052);
-		// 	// 收款名
-		// 	ctx.font = "70px '黑体'";
-		// 	ctx.fillStyle = recTextColor;
-		// 	ctx.fillText("扫码向“" + recName + "”付款", 450, 120);
-		// 	// 下文本
-		// 	ctx.font = "80px '华文新魏'";
-		// 	ctx.fillStyle = bottomTextColor;
-		// 	ctx.fillText("万能收款码", 450, 1183);
-
-		// 	var canvas = document.getElementById("canvas");
-		// 	var ctx = canvas.getContext('2d');
-		// 	var canvasOld = document.getElementsByTagName('canvas')[0];
-		// 	ctx.drawImage(canvasOld, 166, 224);
-
-
-		//	// 生成三个图标
-		// 	new Promise((resolve, reject) => {
-		// 		$("#endImg").show();
-		// 		var imgWechat = new Image();
-		// 		imgWechat.src = "imgs/icons/wechat.png";
-		// 		imgWechat.onload = function () {
-		// 			ctx.drawImage(imgWechat, 170, 872, 123, 123);
-		// 			resolve();
-
-		// 		}
-		// 	}).then(() => {
-		// 		return new Promise((resolve, reject) => {
-		// 			var imgAlipay = new Image();
-		// 			imgAlipay.src = "imgs/icons/alipay.png";
-		// 			imgAlipay.onload = function () {
-		// 				ctx.drawImage(imgAlipay, 390, 872, 123, 123);
-		// 				resolve();
-		// 			}
-		// 		})
-		// 	}).then(() => {
-		// 		return new Promise((resolve, reject) => {
-		// 			var imgQQ = new Image();
-		// 			imgQQ.src = "imgs/icons/QQ.png";
-		// 			imgQQ.onload = function () {
-		// 				ctx.drawImage(imgQQ, 611, 872, 123, 123);
-		// 				//canvas转成图片，以便移动端长按保存
-		// 				var image = new Image();
-		// 				image.src = canvas.toDataURL("image/png");
-		// 				$(".form-horizontal img").attr("src", image.src);
-		// 				$("canvas").hide();
-		// 				$("#endImg").show();
-		// 				// 显示下载按钮
-		// 				$("#downImg").show();
-		// 				// 显示颜色修改区域
-		// 				$("#adjustColor").show();
-		// 				resolve();
-		// 			}
-		// 		})
-		// 	}).then(() => {
-		// 		// 判断是否重复生成
-		// 		autoBottom();
-		// 	})
-
-
-		// }
 
 		// 生成收款码（其他样式 背景图已指定）
 		function makeDiyBg(element, qrWidth, qrHeight, url, foreground, background, imgUrl, imgWidth, imgHeight, font, fontColor, recName, recNameLeft, recNameTop, qrLeft, qrTop) {
@@ -418,10 +266,6 @@ $(function () {
 				dataType: "JSONP",
 				success: function (res) {
 					// 生成缩网址二维码
-					// console.log('//api.weibo.com/2/short_url/shorten.json?source=2849184197&url_long=1' + urlEncode(longUrl));
-					// console.log("短网址:"+ res.data.urls[0].url_short);
-
-					// makeBg(undefined, undefined, undefined, undefined, undefined, res.data.urls[0].url_short);
 					layer.closeAll();
 					// 获取当前被选中样式图片地址
 					var nowUrl = document.querySelector(".swiper-slide-active").style.backgroundImage.replace('url(', '').replace(')', '').replace('"', '').replace('"', '');
@@ -486,6 +330,7 @@ $(function () {
 				},
 				error: function (jqXHR, textStatus, errorThrown) {
 					if (jqXHR.statusText == "error") {
+						// 生成原链接二维码
 						layer.closeAll();
 						// 获取当前被选中样式图片地址
 						var nowUrl = document.querySelector(".swiper-slide-active").style.backgroundImage.replace('url(', '').replace(')', '').replace('"', '').replace('"', '');
@@ -564,6 +409,7 @@ $(function () {
 
 
 	})
+
 	// 底部导航条自适应
 	function autoBottom() {
 		if ($(document.body).height() > $(window).height()) {
