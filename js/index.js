25e66f0516c67d8f9df017a8996cf847b14dc4ca
bwmelon <1454490647@qq.com>
2019-09-22 20:23:16 +0800

    :sparkles:新增五款艺术收款码模板

--- js/index.js
+++ js/index.js
@@ -6,38 +6,31 @@ console.log("%c%cBUG反馈群：%c789371353(遇到问题尽量自己解决，因
 
 
 $(function () {
+	layui.use(['layer'], function () {
 
-	// 滚动样式
-	var swiper = new Swiper('.swiper-container', {
-		initialSlide: 0,
-		effect: 'coverflow',
-		grabCursor: true,
-		centeredSlides: true,
-		slidesPerView: 'auto',
-		loop: true,
-		mousewheel: true,
-		coverflowEffect: {
-			rotate: 50,
-			stretch: 0,
-			depth: 100,
-			modifier: 1,
-			slideShadows: true,
-		},
-		pagination: {
-			el: '.swiper-pagination',
-		},
-	});
-
-	autoBottom();
-
-
-
-
-	layui.use(['layer', 'colorpicker'], function () {
-		var isrepeated = false;
 		var layer = layui.layer;
-		var colorpicker = layui.colorpicker;
-
+		autoBottom();
+		// 模板滚动样式
+		new Swiper('.swiper-container', {
+			initialSlide: 0,
+			effect: 'coverflow',
+			grabCursor: true,
+			centeredSlides: true,
+			slidesPerView: 'auto',
+			loop: true,
+			mousewheel: true,
+			coverflowEffect: {
+				rotate: 50,
+				stretch: 0,
+				depth: 100,
+				modifier: 1,
+				slideShadows: true,
+			},
+			pagination: {
+				el: '.swiper-pagination',
+			},
+		});
+		
 		// 弹窗广告,不需要则删除该段代码
 		layer.open({
 			type: 1,
@@ -52,88 +45,10 @@ $(function () {
 			}
 		});
 
-
-		//获取qq收款码链接
-		function getUrl_qq(e, param) {
-			analyticCode.getUrl(
-				param, e,
-				function (url) {
-					if (url == 'error decoding QR Code') {
-						layer.msg('二维码解析失败，请重新上传', {
-							time: 3000,
-							icon: 5
-						});
-					} else if (url.indexOf('qianbao') == '-1') {
-						layer.msg('该收款码不是QQ收款码，请上传QQ收款码', {
-							time: 3000,
-							icon: 5
-						});
-					} else {
-						layer.msg('上传成功', {
-							time: 3000,
-							icon: 6
-						});
-						document.getElementById('qq').value = url;
-					}
-				}
-			)
-		}
-
-		//获取微信收款码链接
-		function getUrl_wechat(e, param) {
-			analyticCode.getUrl(
-				param, e,
-				function (url) {
-					if (url == 'error decoding QR Code') {
-						layer.msg('二维码解析失败，请重新上传', {
-							time: 3000,
-							icon: 5
-						});
-					} else if (url.indexOf('wxp') == '-1') {
-						layer.msg('该收款码不是微信收款码，请上传微信收款码', {
-							time: 3000,
-							icon: 5
-						});
-					} else {
-						layer.msg('上传成功', {
-							time: 3000,
-							icon: 6
-						});
-						document.getElementById('wechat').value = url;
-					}
-				}
-			)
-		}
-
-		//获取支付宝收款码链接
-		function getUrl_ali(e, param) {
-			analyticCode.getUrl(
-				param, e,
-				function (url) {
-					if (url == 'error decoding QR Code') {
-						layer.msg('二维码解析失败，请重新上传', {
-							time: 3000,
-							icon: 5
-						});
-					} else if (url.indexOf('ALIPAY') == '-1' && url.indexOf('alipay') == '-1') {
-						layer.msg('该收款码不是支付宝收款码，请上传支付宝收款码', {
-							time: 3000,
-							icon: 5
-						});
-					} else {
-						layer.msg('上传成功', {
-							time: 3000,
-							icon: 6
-						});
-						document.getElementById('ali').value = url;
-					}
-
-
-				}
-			)
-		}
-
-		// url编码
+		/**
+		 * url编码
+		 * @param {String} String 
+		 */
 		function urlEncode(String) {
 			return encodeURIComponent(String).replace(/'/g, "%27").replace(/"/g, "%22");
 		}
@@ -315,13 +230,33 @@ $(function () {
 
 		});
 
-		// 生成收款码（其他样式 背景图已指定）
-		function makeDiyBg(element, qrWidth, qrHeight, url, foreground, background, imgUrl, imgWidth, imgHeight, font, fontColor, recName, recNameLeft, recNameTop, qrLeft, qrTop) {
+
+
+		/**
+		 * 生成普通样式二维码
+		 * @param {HTMLElement} element 
+		 * @param {Number} qrWidth 二维码宽度
+		 * @param {Number} qrHeight 二维码高度
+		 * @param {String} tinyurl 二维码地址、内容
+		 * @param {String} foreground 前景色
+		 * @param {String} background 背景色
+		 * @param {String} imgUrl 背景地址
+		 * @param {Number} imgWidth 图片宽度
+		 * @param {Number} imgHeight 图片高度
+		 * @param {String} font 字体
+		 * @param {String} fontColor 颜色 
+		 * @param {String} recName 收款名
+		 * @param {Number} recNameLeft 收款名左侧距离
+		 * @param {Number} recNameTop 收款名顶部距离
+		 * @param {Number} qrLeft 二维码左侧距离
+		 * @param {Number} qrTop 二维码顶部距离
+		 */
+		function makeDiyBg(element, qrWidth, qrHeight, tinyurl, foreground, background, imgUrl, imgWidth, imgHeight, font, fontColor, recName, recNameLeft, recNameTop, qrLeft, qrTop) {
 			$(element).qrcode({
 				render: "canvas",
 				width: qrWidth,
 				height: qrHeight,
-				text: url,
+				text: tinyurl,
 				foreground: foreground,
 				background: background
 			});
@@ -360,7 +295,6 @@ $(function () {
 		}
 
 		$("#make").click(function () {
-
 			// 重置canvas里面的二维码
 			$("#code").empty();
 
@@ -374,133 +308,31 @@ $(function () {
 				time: 10 * 1000
 			});
 
-			//由于原来生成的链接太长，生成的二维码太密集，所以通过新浪短网址(https://open.weibo.com/wiki/%E5%BE%AE%E5%8D%9AAPI#.E7.9F.AD.E9.93.BE)生成短网址
+			//由于原来生成的链接太长，生成的二维码太密集，所以通过腾讯/新浪生成短网址
 			$.ajax({
 				type: 'get',
 				url: 'long2tiny.php?&url_long=' + urlEncode(urlEncode(longUrl)),
-				success: function (res) {				
+				success: function (tinyurl) {
 					// 生成缩网址二维码
 					layer.closeAll();
 					// 获取当前被选中样式图片地址
 					var nowUrl = document.querySelector(".swiper-slide-active").style.backgroundImage.replace('url(', '').replace(')', '').replace('"', '').replace('"', '');
-					
+
 					// 判断index.html引用图片的方式
 					var nowName = $(".swiper-slide-active").attr("mould-name") ? $(".swiper-slide-active").attr("mould-name") : nowUrl.split("/").pop().replace(".png", "");
-					
-					// 从config.json文件中读取样式配置
-					var getJson = $.ajax({
-						type: 'get',
-						url: "config.json",
-						dataType: "JSON",
-						async: true,
-						success: function () {
-							var styleName = nowName;
-							var config = JSON.parse(getJson.responseText);
-							var qrWidth = config[styleName].qrWidth;
-							var qrHeight = config[styleName].qrHeight;
-							var foreground = config[styleName].foreground;
-							var background = config[styleName].background;
-							var imgWidth = config[styleName].imgWidth;
-							var imgHeight = config[styleName].imgHeight;
-							var font = config[styleName].font;
-							var fontColor = config[styleName].fontColor;
-							var recNameLeft = config[styleName].recNameLeft;
-							var recNameTop = config[styleName].recNameTop;
-							var qrLeft = config[styleName].qrLeft;
-							var qrTop = config[styleName].qrTop;
-							makeDiyBg("#code", qrWidth, qrHeight, res, foreground, background, nowUrl, imgWidth, imgHeight, font, fontColor, document.getElementById("recName").value, recNameLeft, recNameTop, qrLeft, qrTop);
-							autoBottom();
-							//页面层-收款码
-							setTimeout(function () {
-								layer.open({
-									type: 1,
-									title: false,
-									closeBtn: 2,
-									anim: 0,
-									zIndex: 5,
-									area: 'auto',
-									shadeClose: true,
-									content: $('#endImg'),
-									btn: ['长按图片或点我保存'],
-									btnAlign: 'c',
-									yes: function () {
-										var img = document.getElementById('endImg');
-										var url = img.src;
-										var a = document.createElement('a');
-										var event = new MouseEvent('click');
-										a.download = '万能收款码-' + document.getElementById("recName").value;
-										a.href = url;
-										a.dispatchEvent(event);
-									}
-								});
-							}, 100);
-						}
-					})
 
-					layer.msg('收款码生成成功，请长按或右击保存', {
-						time: 2000,
-						icon: 6
-					});
+					// 判断是否为艺术码
+					var dataArt = document.querySelector(".swiper-slide-active").dataset.art;
+					
+					if (dataArt) {
+						makeArtQrcode(dataArt, tinyurl);
+					} else {
+						makeCommonQrcode(nowUrl, nowName, tinyurl);
+					}
 				},
 				error: function (jqXHR, textStatus, errorThrown) {
-					if (jqXHR.statusText == "error") {
-						// 生成原链接二维码
-						layer.closeAll();
-						// 获取当前被选中样式图片地址
-						var nowUrl = document.querySelector(".swiper-slide-active").style.backgroundImage.replace('url(', '').replace(')', '').replace('"', '').replace('"', '');
-
-						// 获取当前被选中样式图片名称 获取到的名称用来在配置文件中查找
-						var nowName = nowUrl.split("/").pop().replace(".png", "");
-
-						// 从config.json文件中读取样式配置
-						var getJson = $.ajax({
-							type: 'get',
-							url: "config.json",
-							async: true,
-							success: function () {
-								var styleName = nowName;
-								var config = JSON.parse(getJson.responseText);
-								var qrWidth = config[styleName].qrWidth;
-								var qrHeight = config[styleName].qrHeight;
-								var foreground = config[styleName].foreground;
-								var background = config[styleName].background;
-								var imgWidth = config[styleName].imgWidth;
-								var imgHeight = config[styleName].imgHeight;
-								var font = config[styleName].font;
-								var fontColor = config[styleName].fontColor;
-								var recNameLeft = config[styleName].recNameLeft;
-								var recNameTop = config[styleName].recNameTop;
-								var qrLeft = config[styleName].qrLeft;
-								var qrTop = config[styleName].qrTop;
-								makeDiyBg("#code", qrWidth, qrHeight, longUrl, foreground, background, nowUrl, imgWidth, imgHeight, font, fontColor, document.getElementById("recName").value, recNameLeft, recNameTop, qrLeft, qrTop);
-								autoBottom();
-								//页面层-收款码
-								setTimeout(function () {
-									layer.open({
-										type: 1,
-										title: false,
-										closeBtn: 2,
-										anim: 0,
-										zIndex: 5,
-										area: 'auto',
-										shadeClose: true,
-										content: $('#endImg'),
-										btn: ['长按图片或点我保存'],
-										btnAlign: 'c',
-										yes: function () {
-											var img = document.getElementById('endImg');
-											var url = img.src;
-											var a = document.createElement('a');
-											var event = new MouseEvent('click');
-											a.download = '万能收款码-' + document.getElementById("recName").value;
-											a.href = url;
-											a.dispatchEvent(event);
-										}
-									});
-								}, 100);
-							}
-						})
-						layer.msg('收款码生成成功，但是二维码简化失败，请长按或右击保存', {
+					if (textStatus == "error") {
+						layer.msg('收款码生成失败，请联系网站管理员', {
 							time: 3000,
 							icon: 5
 						});
@@ -509,26 +341,137 @@ $(function () {
 			})
 		})
 
-		//点击按钮保存图片
-		$("#downImg").click(function () {
-			var img = document.getElementById('endImg');
-			var url = img.src;
-			var a = document.createElement('a');
-			var event = new MouseEvent('click');
-			a.download = '万能收款码-' + document.getElementById("recName").value;
-			a.href = url;
-			a.dispatchEvent(event);
-		})
+		// 底部导航条自适应
+		function autoBottom() {
+			if ($(document.body).height() > $(window).height()) {
+				// 取消底部导航栏固定
+				$("footer").removeClass(" navbar-fixed-bottom");
+				// 修改底部导航栏样式
+				$("footer").addClass("afterMakeNav");
+			}
+		}
+
+		/**
+		 * 生成普通二维码，从config.json文件中读取样式配置
+		 */
+		function makeCommonQrcode(nowUrl, nowName, tinyurl) {
+			getCommonQrcodeCfg().then(res => {
+				var styleName = nowName;
+				var config = res;
+				var qrWidth = config[styleName].qrWidth;
+				var qrHeight = config[styleName].qrHeight;
+				var foreground = config[styleName].foreground;
+				var background = config[styleName].background;
+				var imgWidth = config[styleName].imgWidth;
+				var imgHeight = config[styleName].imgHeight;
+				var font = config[styleName].font;
+				var fontColor = config[styleName].fontColor;
+				var recNameLeft = config[styleName].recNameLeft;
+				var recNameTop = config[styleName].recNameTop;
+				var qrLeft = config[styleName].qrLeft;
+				var qrTop = config[styleName].qrTop;
+				makeDiyBg("#code", qrWidth, qrHeight, tinyurl, foreground, background, nowUrl, imgWidth, imgHeight, font, fontColor, document.getElementById("recName").value, recNameLeft, recNameTop, qrLeft, qrTop);
+				autoBottom();
+				setTimeout(function () {
+					alertQrcode("#endImg");
+					alertInfo();
+				}, 100);
+
+			})
+		}
+
+		/**
+		 * 获取普通码信息，从art-config.json文件中读取样式配置
+		 */
+		async function getCommonQrcodeCfg() {
+			return await fetch("config.json")
+				.then(res => {
+					return res.json();
+				});
+		}
+
+		/**
+		 * 获取艺术码信息
+		 */
+		async function getArtQrcodeCfg() {
+			return await fetch("art-config.json")
+				.then(res => {
+					return res.json();
+				});
+		}
+
+		// 生成艺术二维码
+		function makeArtQrcode(mouldName, tinyurl) {
+			$("#qrcode").empty();
+			getArtQrcodeCfg().then(res => {
+				res[mouldName].materials.border = "./imgs/bgimgs/" + mouldName + ".png";
+				var options = {
+					text: tinyurl,
+					width: res[mouldName].imgWidth,
+					height: res[mouldName].imgHeight,
+					codeWidth: res[mouldName].qrWidth,
+					codeHeight: res[mouldName].qrHeight,
+					top: res[mouldName].qrTop,
+					left: res[mouldName].qrLeft,
+					materials: res[mouldName].materials
+				}
+				new QRCodeA(document.getElementById("qrcode"), options, callBack);
+				$("#qrcode").show();
+			})
+		}
+
+		/**
+		 * 弹出生成的收款码
+		 * @param {String} element 需要弹出的元素 
+		 */
+		function alertQrcode(element) {
+			layer.open({
+				type: 1,
+				title: false,
+				closeBtn: 2,
+				anim: 0,
+				zIndex: 5,
+				area: 'auto',
+				shadeClose: true,
+				content: $(element),
+				btn: ['长按图片或点我保存'],
+				btnAlign: 'c',
+				yes: function () {
+					var img = document.querySelector(element);
+					var url = img.src;
+					var a = document.createElement('a');
+					var event = new MouseEvent('click');
+					a.download = '万能收款码-' + document.getElementById("recName").value;
+					a.href = url;
+					a.dispatchEvent(event);
+				}
+			});
+		}
+
+		/**
+		 * 生成成功后弹出提示信息
+		 */
+		function alertInfo() {
+			layer.msg('收款码生成成功，请长按或右击保存', {
+				time: 2000,
+				icon: 6
+			});
+		}
+
+		/**
+		 * 艺术二维码回调函数
+		 * @param {String} status [loaded|success]
+		 */
+		function callBack(status) {
+			if (status == "success") {
+				setTimeout(() => {
+					layer.closeAll();
+					alertInfo();
+					alertQrcode("#art-qrcode");
+				}, 10);
+			}
+		}
 	})
 
-	// 底部导航条自适应
-	function autoBottom() {
-		if ($(document.body).height() > $(window).height()) {
-			// 取消底部导航栏固定
-			$("footer").removeClass(" navbar-fixed-bottom");
-			// 修改底部导航栏样式
-			$("footer").addClass("afterMakeNav");
-		}
-	}
-
+	
 });
