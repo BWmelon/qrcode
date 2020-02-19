98a0ba94ed6db5a139c7ac5735fa1381c1f455c9
bwmelon <1454490647@qq.com>
2020-02-19 11:45:03 +0800

    :ambulance:更换短网址api

--- long2tiny.php
+++ long2tiny.php
@@ -8,34 +8,41 @@ if ($_GET['url_long']) {
 }
 
 // 短网址api接口
-// 第三方(优启梦API)生成腾讯 2019/11/14 更新 https://api.uomg.com/api/long2dwz?dwzapi=urlcn&url=?
-// 腾讯 http://sa.sogou.com/gettiny?url= (已失效)
-// 新浪 https://api.t.sina.com.cn/short_url/shorten.json?source=2849184197&url_long= (已失效)
+// 腾讯 http://shorturl.8446666.sojson.com/qq/shorturl?url= 
+// 新浪 http://shorturl.8446666.sojson.com/sina/shorturl?url= (备用)
 
-$api = "https://api.uomg.com/api/long2dwz?dwzapi=urlcn&url=";
+$api = "http://shorturl.8446666.sojson.com/qq/shorturl?url=";
 
 // 请求地址
-$url = $api . $url_long;   
+$url = $api . $url_long;
 
-// if ($api == "http://sa.sogou.com/gettiny?url=") {
-//     $tinyurl = file_get_contents($url);
-// } else if($api == "https://api.t.sina.com.cn/short_url/shorten.json?source=2849184197&url_long=") {
-//     $res = json_decode(file_get_contents($url), true);
-//     $tinyurl = str_replace("http", "https", $res[0]["url_short"]);
-// } else {
-//     $tinyurl = "Api error.";
-// }
-
-if($api == "https://api.uomg.com/api/long2dwz?dwzapi=urlcn&url=") {
+// 腾讯
+if ($api == "http://shorturl.8446666.sojson.com/qq/shorturl?url=") {
     $res = json_decode(file_get_contents($url), true);
-    if($res["code"] == 1) {
-        $tinyurl = $res["ae_url"];
+    if ($res["status"] == 200) {
+        $tinyurl = $res["shorturl"];
     } else {
-        $tinyurl = '二维码生成失败';    
+        // $tinyurl = 'Short URL generation failed, the reason may be that the request frequency exceeds 300 times / h, and the IP is blocked for one day.';    
+        
+        // 腾讯短网址被限制请求时使用新浪短网址
+        $res = json_decode(file_get_contents($url), true);
+        if ($res["status"] == 200) {
+            $tinyurl = str_replace("http", "https", $res["shorturl"]);
+        } else {
+            $tinyurl = 'Short URL generation failed, the reason may be that the request frequency exceeds 300 times / h, and the IP is blocked for one day.';
+        }
     }
-    
 }
 
-echo $tinyurl;
+// 新浪
+// if($api == "http://shorturl.8446666.sojson.com/sina/shorturl?url=") {
+//     $res = json_decode(file_get_contents($url), true);
+//     if($res["status"] == 200) {
+//         $tinyurl = str_replace("http", "https", $res["shorturl"]);
+//     } else {
+//         $tinyurl = 'Short URL generation failed, the reason may be that the request frequency exceeds 300 times / h, and the IP is blocked for one day.';    
+//     }
 
-?>
+// }
+
+echo $tinyurl;
