edaea21c8593b0691c6e216dfe20dc0d97f406eb
bwmelon <1454490647@qq.com>
2020-05-04 12:44:44 +0800

    :ambulance:更换短网址api

--- long2tiny.php
+++ long2tiny.php
@@ -8,41 +8,23 @@ if ($_GET['url_long']) {
 }
 
 // 短网址api接口
-// 腾讯 http://shorturl.8446666.sojson.com/qq/shorturl?url= 
-// 新浪 http://shorturl.8446666.sojson.com/sina/shorturl?url= (备用)
+// 腾讯 http://shorturl.8446666.sojson.com/qq/shorturl?url= （失效）
+// 新浪 http://shorturl.8446666.sojson.com/sina/shorturl?url= 
 
-$api = "http://shorturl.8446666.sojson.com/qq/shorturl?url=";
+$api = "http://shorturl.8446666.sojson.com/sina/shorturl?url=";
 
 // 请求地址
 $url = $api . $url_long;
 
-// 腾讯
-if ($api == "http://shorturl.8446666.sojson.com/qq/shorturl?url=") {
-    $res = json_decode(file_get_contents($url), true);
-    if ($res["status"] == 200) {
-        $tinyurl = $res["shorturl"];
-    } else {
-        // $tinyurl = 'Short URL generation failed, the reason may be that the request frequency exceeds 300 times / h, and the IP is blocked for one day.';    
-        
-        // 腾讯短网址被限制请求时使用新浪短网址
-        $res = json_decode(file_get_contents($url), true);
-        if ($res["status"] == 200) {
-            $tinyurl = str_replace("http", "https", $res["shorturl"]);
-        } else {
-            $tinyurl = 'Short URL generation failed, the reason may be that the request frequency exceeds 300 times / h, and the IP is blocked for one day.';
-        }
-    }
-}
-
 // 新浪
-// if($api == "http://shorturl.8446666.sojson.com/sina/shorturl?url=") {
-//     $res = json_decode(file_get_contents($url), true);
-//     if($res["status"] == 200) {
-//         $tinyurl = str_replace("http", "https", $res["shorturl"]);
-//     } else {
-//         $tinyurl = 'Short URL generation failed, the reason may be that the request frequency exceeds 300 times / h, and the IP is blocked for one day.';    
-//     }
+if($api == "http://shorturl.8446666.sojson.com/sina/shorturl?url=") {
+    $res = json_decode(file_get_contents($url), true);
+    if($res["status"] == 200) {
+        $tinyurl = str_replace("http", "https", $res["shorturl"]);
+    } else {
+        $tinyurl = 'Short URL generation failed, the reason may be that the request frequency exceeds 300 times / h, and the IP is blocked for one day.';    
+    }
 
-// }
+}
 
 echo $tinyurl;
